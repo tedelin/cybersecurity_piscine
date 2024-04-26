c4beaabb170a6752914ca9644b69d13524c7c367
Tanguy EDELIN <tedelin@paul-f4Br4s8.clusters.42paris.fr>
2024-04-26 18:31:54 +0200

    fix already visited and args -rl

--- spider.py
+++ spider.py
@@ -9,8 +9,8 @@ from datetime import datetime
 parser = argparse.ArgumentParser(description="Spider extract all the images from a website")
 parser.add_argument("url", help="The URL of the website to extract images")
 parser.add_argument("-r", action="store_true", help="Extract images recursively")
-parser.add_argument("-l", type=int, help="The maximum number of levels to extract images")
-parser.add_argument("-p", help="The path to save the images")
+parser.add_argument("-l", type=int, default=5, metavar='N', help="The maximum number of levels to extract images")
+parser.add_argument("-p", metavar='directory', help="The path to save the images")
 args = parser.parse_args()
 
 file_extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
@@ -18,12 +18,9 @@ already_visited = []
 img_folder = "data" if not args.p else args.p
 if not os.path.exists(img_folder):
     os.makedirs(img_folder)
+if not args.r:
+	args.l = 0
 
-level = 5
-if args.r:
-	level = 5
-if args.l and args.r:
-	level = args.l
 def generate_unique_name(ext):
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     unique_id = uuid.uuid4().hex[:6] 
@@ -57,10 +54,11 @@ def spider(url, lvl):
 					f.write(image_response.content)
 	for page in pages:
 		new_url = page.get("href")
-		if new_url and new_url not in already_visited:
+		if new_url:
 			if (new_url.startswith("http") == 0):
 					new_url = url + new_url
-			spider(new_url, lvl - 1)
+			if (new_url not in already_visited):
+				spider(new_url, lvl - 1)
 
-print(level)
-spider(args.url, level)
+print(args.l)
+spider(args.url, args.l)
