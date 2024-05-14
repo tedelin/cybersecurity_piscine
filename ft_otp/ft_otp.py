19276572f8477e5a6e04a7a7de24930e5f005490
Tanguy Edelin <tedelin@Tanguys-Laptop.local>
2024-05-14 11:19:46 +0200

    file organization and ft_otp finish

--- ft_otp/ft_otp.py
+++ ft_otp/ft_otp.py
@@ -1,12 +1,25 @@
 import time
+import string
 import hmac
 import hashlib
-# import argparse
+import argparse
+from cryptography.fernet import Fernet
 
-# parser = argparse.ArgumentParser(description="Spider extract all the images from a website")
-# parser.add_argument("-g", type=str, default=5, metavar='N', help="The maximum number of levels to extract images")
-# parser.add_argument("-k", type=str, default=5, metavar='N', help="The maximum number of levels to extract images")
-# args = parser.parse_args()
+parser = argparse.ArgumentParser(description="ft_otp generate one time password")
+parser.add_argument("-g", type=str, metavar='filename', help="register a new key")
+parser.add_argument("-k", type=str, metavar='filename', help="The key used to generate the one time password")
+args = parser.parse_args()
+key = "N1sUo4OdYvEqA9Xuf1XBwcdBOwoBVqTMtQALNn96nD0="
+
+def encrypt_key(key, plaintext):
+    cipher = Fernet(key)
+    encrypted_text = cipher.encrypt(plaintext.encode())
+    return encrypted_text
+
+def decrypt_key(key, ciphertext):
+    cipher = Fernet(key)
+    decrypted_text = cipher.decrypt(ciphertext).decode()
+    return decrypted_text
 
 def generate(key, codeDigits, time_step=30):
 	result = ""
@@ -22,5 +35,30 @@ def generate(key, codeDigits, time_step=30):
 		result = "0" + result
 	return result
 
-key = "5cc2008fe6aaeb554debe6de19bc4ac3833f02468612726afe3a381c325bdfb6"
-print(generate(key, 6))
+def register_key(filename):
+	try:
+		with open(filename, 'r') as f:
+			hex_key = f.read()
+		if len(hex_key) < 64:
+			print("./ft_otp: error: key must be 64 hexadecimal characters.")
+			return
+		if (all(c in string.hexdigits for c in hex_key) == 0):
+			print("./ft_otp: error: key must be 64 hexadecimal characters.")
+			return
+		encrypted_key = encrypt_key(key, hex_key)
+		with open("ft_otp.key", 'wb') as f:
+			f.write(encrypted_key)
+		print("Key was successfully saved in ft_otp.key.")
+	except Exception as e:
+		print("An error occurred: ", e)
+
+if args.g:
+	register_key(args.g)
+elif args.k:
+	try:
+		with open(args.k, 'rb') as f:
+			encrypted_key = f.read()
+		hex_key = decrypt_key(key, encrypted_key)
+		print(generate(hex_key, 6))
+	except Exception as e:
+		print("An error occurred: ", e)
